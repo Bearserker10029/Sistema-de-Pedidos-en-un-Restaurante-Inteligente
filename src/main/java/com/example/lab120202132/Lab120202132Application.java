f4e21cbd246afdd76aff89201e431727ae81cb7e
Bearserker10029 <kentjianbin@gmail.com>
2026-04-09 19:24:13 -0500

    Initial commit

---
+++ src/main/java/com/example/lab120202132/Lab120202132Application.java
@@ -0,0 +1,13 @@
+package com.example.lab120202132;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab120202132Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab120202132Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab120202132/Lab120202132ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab120202132;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab120202132ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
